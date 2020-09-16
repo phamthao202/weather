8f045bc909f1b0f4ed748414138b7d1b17f3c230
Julia Pham <phamthao202@gmail.com>
2020-09-16 13:39:42 +0700

    first

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weather: null,
+    };
+  }
+  getWeather = async () => {
+    let apiKey = process.env.REACT_APP_APIKEY;
+
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=paris&appid=${apiKey}&units=metric`;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log("data", data);
+    this.setState({ weather: data });
+  };
+  componentDidMount() {
+    this.getWeather();
+  }
+  render() {
+    return <h1>{this.state.weather && this.state.weather.main.temp}</h1>;
+  }
+}
